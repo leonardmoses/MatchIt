45262a4a50e25573487e60a2dbed1de273379967
leonardmoses <leonard.moses@anemote.com>
2021-11-17 15:01:26 -0500

    added backs/covers for cards via url. allowed cards to be hidden through css.

--- pages/Cards.js
+++ pages/Cards.js
@@ -2,7 +2,7 @@ import Layout from "../components/Layout";
 import styleCards from "../styles/Cards.module.scss"
 
 export const getStaticProps = async () => {
-    const res = await fetch('https://deckofcardsapi.com/api/deck/new/draw/?count=16');
+    const res = await fetch('https://deckofcardsapi.com/api/deck/new/draw/?count=8');
     const data = await res.json();
 
     return {
@@ -13,24 +13,41 @@ export const getStaticProps = async () => {
 
 
 const Cards = ({ deckInfo }) => {
-    // console.log(deckInfo)
-    const deck = deckInfo.cards
+
+    let predeck = deckInfo.cards
+
+    // console.log(predeckInfo)
+    let deck = [];
+
+    predeck.forEach(element => {
+        deck.push(element);
+        deck.push(element);
+    });
+
+    // console.log(deck)
+    // console.log(deck.length)
+
+    function shuffle(array) {
+        array.sort(() => Math.random() - 0.5);
+    }
+    shuffle(deck)
+
     console.log(deck)
-    console.log(deck.length)
 
 
     return ( 
         <Layout>
             <div className={styleCards.cardinfo}>
-                <h1>Deck of Cards</h1>
-                <h3>Remaining Cards in the Deck: {deckInfo.remaining}</h3>
+                <h1>{deck.length} cards</h1>
+                <h3 hidden>Remaining Cards in the Deck: {deckInfo.remaining}</h3>
             </div>
 
             <div className={styleCards.grid}>
                 {deck.map(card => (  
-                    <div key={card.code}>
+                    <div key={card.code} className={styleCards.cardcontainer}>
                         <h4>{card.value} of {card.suit}</h4>
-                        <img src={card.image} alt={`The ${card.value} of ${card.suit}`}/>
+                        <img src={card.image} alt={`The ${card.value} of ${card.suit}`} className={styleCards.cardfront}/>
+                        <img src="https://media.istockphoto.com/photos/bicycle-rider-back-playing-card-design-picture-id157772536?k=20&m=157772536&s=170667a&w=0&h=46bM0a2wuwcddiOzNOHTfS9PcUzjXwNTTCy33SrkC_0=" className={styleCards.cardback}/>
                     </div>
                 ))}
             </div>
